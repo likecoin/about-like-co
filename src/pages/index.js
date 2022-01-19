675f2f4053e38a98da18bd76788ed5e4e5cd09fa
AuroraHuang22 <aurora90376@gmail.com>
2022-01-19 18:04:26 +0800

    💬 Update homepage copywriting

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,10 +42,10 @@ const IndexPage = () => {
           </div>
         </div>
         <div className="relative text-center p-[24px]">
-          <GradientText tag="h1" className="text-[42px] sm:text-[64px] md:text-[80px] leading-tight">Decentralized Publishing</GradientText>
-          <p className="text-[20px] mt-[24px]">
-            Digital evidence can now be secured, validated, and made readily available in perpetuity with #DePub.
-          </p>
+          <GradientText tag="h1" className="text-[42px] sm:text-[64px] md:text-[80px] leading-tight">Decentralize Publishing</GradientText>
+          <h2 className="text-[32px] font-bold font-display mt-[24px]">
+            Own your content forever.
+          </h2>
         </div>
       </section>
 
@@ -55,7 +55,7 @@ const IndexPage = () => {
           <img className="absolute top-0 left-0 object-cover w-full h-full" src="/uploads/airdrop/background-sm.png" />
           <img className="absolute top-0 left-0 object-cover w-full h-full" src="/uploads/airdrop/background-planets.png" />
           <div className="z-10 mb-[24px] md:mr-[24px] md:mb-0">
-            <img className="block w-[353px] h-[56px] mb-[8px]" src="/uploads/airdrop/title-likecoin.png" />
+            <img className="block w-full h-full max-w-[353px] max-h-[56px] object-contain mb-[8px]" src="/uploads/airdrop/title-likecoin.png" alt="airdrop" />
             <p className=" text-[20px] text-dark-gray">LikeCoin airdrop is coming. Check out the amount you can claim now!</p>
           </div>
           <div className="flex-shrink-0 ml-[12px] z-10">
@@ -66,17 +66,17 @@ const IndexPage = () => {
               target="_blank"
               rel="referrer"
             >
-              <img className="mr-[12px]" src="/uploads/airdrop/claim.svg" />
+              <img className="mr-[12px]" src="/uploads/airdrop/claim.svg" alt="airdrop-claim" />
               <p className="whitespace-nowrap text-airdrop-gold text-[16px]">Claim Airdrop</p>
               <img className="ml-[12px]" src="/uploads/airdrop/arrowright.svg" />
             </Button>
           </div>
         </div>
-      <img className="absolute top-[50px] left-[38%] w-[244px]" src="/uploads/airdrop/background-planet.png" />
+        <img className="absolute top-[50px] left-[38%] w-[244px]" src="/uploads/airdrop/background-planet.png" />
       </section>
-      
+
       {/* Sub-Nav Section */}
-      <section className="relative mt-[88px]">
+      <section className="flex flex-col items-center relative mt-[88px]">
         <nav className="flex justify-center">
           <ul className="grid sm:grid-flow-col-dense auto-cols-auto gap-x-[8px] gap-y-[16px]">
             <li>
@@ -85,7 +85,7 @@ const IndexPage = () => {
                 href="https://app.like.co"
                 target="_blank"
                 rel="noreferrer"
-              >Launch app</Button>
+              >Get started</Button>
             </li>
             <li>
               <Button
@@ -113,6 +113,9 @@ const IndexPage = () => {
             </li>
           </ul>
         </nav>
+        <p className="text-[16px] mt-[24px]">
+          Questions? Ask the <a className="underline" href="http://discord.gg/likecoin">community</a>.
+        </p>
       </section>
 
       {/* Statistic Section */}
@@ -129,26 +132,21 @@ const IndexPage = () => {
         </div>
         <div className="relative flex flex-col justify-center flex-grow md:max-w-[66%] p-[24px]">
           <GradientText tag="h2" className="text-[48px] text-center md:text-left">What DePub offers</GradientText>
-          <p className="mt-[24px] text-like-cyan-vlight text-[20px]">LikeCoin aims to empower content ownership, authenticity, and provenance. Creators can register content metadata to guarantee its integrity by acquiring an International Standard Content Number (ISCN). ISCN is a tool to authenticate and track content, just like a digital footprint.</p>
+          <p className="mt-[24px] text-like-cyan-vlight text-[20px]">LikeCoin is a cross-chain protocol and framework for decentralized publishing. It is an application-specific blockchain for building and bridging media to Web 3. </p>
           <ul className="grid md:grid-cols-2	gap-x-[32px] gap-y-[48px] mt-[48px]">
             <li>
-              <WhatDepubOfferItem title="Ready for NFT">
-                Bringing NFT to publishing. Register now, publish anytime. LikeCoin helps secure and freeze content metadata permanently, lock and store it in the content registry. It will allow your metadata to be accessible for future use and distribution. It can also provide a way for the public to verify that the metadata was not changed when a related NFT is minted.
+              <WhatDepubOfferItem title="Not your key, not your content">
+                Always accessible content metadata that guarantees ownership. It makes content easier to retrieve, the same as what gives an NFT its value.
               </WhatDepubOfferItem>
             </li>
             <li>
-              <WhatDepubOfferItem title="Immutable Content Registry">
-                ISCN is a unique identifier to catalog metadata assigned to content such as articles and images. It helps to identify the metadata - creator, date, time, location, version - of the content, akin to a book's distinctive ISBN. 
+              <WhatDepubOfferItem title="LikerDAO">
+                LikeCoin is a community-owned network where LIKE holders make decisions collectively via liquid democracy. Anyone can publish content, build applications and participate in governance.
               </WhatDepubOfferItem>
             </li>
             <li>
-              <WhatDepubOfferItem title="Integrated with Decentralized Storage">
-                Creators, curators, publishers, and readers can easily circulate and distribute content as ISCN allows for more efficient retrieval from databases. LikeCoin chain will dedicate to serve as a metadata registry, and integrate with storage like IPFS and Arweave to provide a one-click solution for users to store their content.
-              </WhatDepubOfferItem>
-            </li>
-            <li>
-              <WhatDepubOfferItem title="Permissionless Participation">
-                LikeCoin operates under liquid democracy as a DAO (Decentralized Autonomous Organization). Anyone can publish content, build applications and participate in governance without gaining permission. No one may censor anyone.
+              <WhatDepubOfferItem title="Ready tools for Web3 native content">
+                A flexible decentralized publication framework (ISCN) that can retrieve data via API, integrate with existing media platforms, and connect with other blockchains. Dapps can easily build integrated tools for creators to produce, manage and own web3 native content. 
               </WhatDepubOfferItem>
             </li>
           </ul>
