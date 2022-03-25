3ce961af05cf0d66724cde2dbf7c5353b4128250
AuroraHuang22 <75730405+AuroraHuang22@users.noreply.github.com>
2022-03-25 16:32:28 +0800

    💄 Add faucet to tokens and plugin list (#28)
    * 💄 Add faucet to token list
    
    * 💄 Add faucet to app list
    
    * 🎨 Compress svg size & fix typo
    
    * ✨ [CMS] Add appList
    
    * 💬 Replace stake.like.co to dao.like.co

--- src/pages/apps.js
+++ src/pages/apps.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { StaticQuery, graphql } from "gatsby"
 
 import AppItem from "../components/AppItem"
 import GradientText from "../components/GradientText"
@@ -6,126 +7,22 @@ import Layout from "../components/Layout"
 import PluginItem from "../components/PluginItem"
 import Seo from "../components/SEO"
 
-const AppsPage = () => {
-  const apps = [
-    {
-      image: '/uploads/apps/iscn.svg',
-      title: "ISCN app",
-      url: "https://app.like.co",
-      isOfficial: true,
-      description: 'Register your content metadata and store the content in the decentralized network with the ISCN web app. ISCN helps you prepare content as NFT.',
-    },
-    {
-      image: '/uploads/apps/liker-land.svg',
-      title: "Liker Land",
-      url: "https://liker.land",
-      isOfficial: true,
-      description: 'The web app for creators and readers to browse, read and bookmark content. Civic Liker — a business tool on Liker Land for creators to run subscription services across platforms.',
-    },
-    {
-      image: '/uploads/apps/matters.svg',
-      title: "Matters",
-      url: "https://matters.news",
-      isOfficial: false,
-      description: 'Decentralized content publishing platform. Direct publish articles to IPFS and utilize the LikeCoin reward mechanism to reward creativity in LIKE.',
-    },
-    {
-      image: '/uploads/apps/liker-social.svg',
-      title: "Liker.Social",
-      url: "https://liker.social",
-      isOfficial: false,
-      description: 'Decentralized microblogs on LikeCoin & Mastodon. Supports likes for rewards, help creators connect with their own communities.',
-    },
-    {
-      image: '/uploads/apps/stake-like-co.svg',
-      title: "Stake.like.co",
-      url: "https://stake.like.co/",
-      isOfficial: false,
-      description: 'The dashboard that helps you to manage, stake and vote with LIKE. Supports login with Keplr wallet, Ledger and Liker ID.',
-    },
-    {
-      image: '/uploads/apps/ddshare.svg',
-      title: "DDShare",
-      url: "https://www.ddshare.io/",
-      isOfficial: false,
-      description: 'Decentralized data-sharing infrastructure. Integrated with LikeCoin and other web3 protocols to offer data sharing services.',
-    },
-  ]
-  const exchandes = [
-    {
-      image: '/uploads/exchanges/osmosis.svg',
-      title: "Osmosis",
-      url: "https://app.osmosis.zone/?from=UST&to=LIKE",
-      isOfficial: true,
-      description: 'Cosmos-based DEX & AMM protocol that designed for cross-chain assets. Easily swap between LIKE and other cross-chain assets.',
-    },
-    {
-      image: '/uploads/exchanges/emeris.svg',
-      title: "Emeris",
-      url: "https://app.emeris.com/asset/nanolike",
-      isOfficial: true,
-      description: 'Cross-chain Defi portal. Integrated with the Gravity DEX protocol on the Cosmos Hub. Easily swap between LIKE and other cross-chain assets.',
-    },
-    {
-      image: '/uploads/exchanges/liquid.svg',
-      title: "Liquid",
-      url: "https://app.liquid.com/exchange/LIKEUSDT",
-      isOfficial: false,
-      description: 'Liquid offers various forms of digital assets trading types. Buy and sell LIKE with USD/ credit card is simple and easy.',
-    },
-  ]
-  const wallets = [
-    {
-      image: '/uploads/wallets/keplr.svg',
-      title: "Keplr",
-      buttonTitle: 'Visit',
-      url: "https://wallet.keplr.app/",
-      description: "Chrome extension wallet that supports LIKE. Keplr supports Google logins, private key imports, and Ledger hardware wallets for maximum usability.",
-    },
-    {
-      image: '/uploads/wallets/liker-land-app.svg',
-      title: "Liker Land app",
-      buttonTitle: 'Visit',
-      url: "https://liker.land/getapp",
-      description: "The mobile app wallet for Likers. Readers can browse and reward content instantly; Creators can easily view followers' statistics, manage and stake LIKE. Wallet connect supports coming soon.",
-    },
-  ]
-  const blockExplors = [
-    {
-      image: '/uploads/block-explors/bigdipper.svg',
-      title: "Big Dipper",
-      buttonTitle: 'Visit',
-      url: "https://likecoin.bigdipper.live",
-    },
-    {
-      image: '/uploads/block-explors/mintscan.svg',
-      title: "Mintscan",
-      buttonTitle: 'Comming Soon',
-      url: "https://www.mintscan.io/cosmos",
-      isDisabled: true,
-    },
-  ]
-  const plugins = [
-    {
-      image: '/uploads/plugins/chrome.svg',
-      title: "Chrome Plugin",
-      buttonTitle: 'Visit',
-      url: "https://chrome.google.com/webstore/detail/liker-land/cjjcemdmkddjbofomfgjedpiifpgkjhe",
-      description: "Browser extension that provides bookmark functions to add any web page to the Liker Land reader. Auto injects the LikeCoin button for Youtube videos."
-    },
-    {
-      image: '/uploads/plugins/wordpress.svg',
-      title: "WordPress Plugin",
-      buttonTitle: 'Visit',
-      url: "https://wordpress.org/plugins/likecoin/",
-      description: "Integrated plugin that bridges WP users to web3. Register content metadata with ISCN. Store and publish content to the decentralized network."
-    },
-  ]
+const AppsPage = ({ items = [], ...props }) => {
+  const apps = items.filter((data) => data.title === "Apps")[0].items;
+  const exchanges = items.filter((data) => data.title === "Exchanges")[0].items;
+  const wallets = items.filter((data) => data.title === "Wallets")[0].items;
+  const blockExplorers = items.filter(
+    (data) => data.title === "BlockExplorers"
+  )[0].items;
+  const plugins = items.filter((data) => data.title === "Plugins")[0].items;
+
   return (
     <Layout className="text-like-cyan-vlight">
       <Seo title="Apps" />
       <section className="flex flex-col items-center mt-[100px] p-[24px]">
-      <GradientText tag="h2" className="text-[48px] text-center md:text-left">Find our apps</GradientText>
+        <GradientText tag="h2" className="text-[48px] text-center md:text-left">
+          Find our apps
+        </GradientText>
         <div className="max-w-[1050px] text-[20px] text-like-cyan-vlight text-center mt-[24px]">
           Discover a range of apps, plugins and communities built in the
           LikeCoin ecosystem by developers and contributors from across the
@@ -140,9 +37,11 @@ const AppsPage = () => {
         </ul>
       </section>
       <section className="flex flex-col items-center mt-[100px] p-[24px]">
-        <GradientText tag="h3" className="text-[32px] text-center">Exchanges</GradientText>
+        <GradientText tag="h3" className="text-[32px] text-center">
+          Exchanges
+        </GradientText>
         <ul className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-x-[24px] gap-y-[40px] mt-[88px]">
-          {exchandes.map((data) => (
+          {exchanges.map((data) => (
             <li key={data.title} className="w-[268px] h-full">
               <AppItem {...data} />
             </li>
@@ -150,7 +49,9 @@ const AppsPage = () => {
         </ul>
       </section>
       <section className="flex flex-col items-center mt-[88px] p-[24px]">
-        <GradientText tag="h3" className="text-[32px] text-center">Wallets</GradientText>
+        <GradientText tag="h3" className="text-[32px] text-center">
+          Wallets
+        </GradientText>
         <ul className="grid md:grid-cols-2 lg:grid-cols-2 gap-x-[24px] gap-y-[40px] mt-[88px]">
           {wallets.map((data) => (
             <li key={data.title} className="w-[324px]">
@@ -160,9 +61,11 @@ const AppsPage = () => {
         </ul>
       </section>
       <section className="flex flex-col items-center mt-[88px] p-[24px]">
-        <GradientText tag="h3" className="text-[32px] text-center">Block Explorers</GradientText>
+        <GradientText tag="h3" className="text-[32px] text-center">
+          Block Explorers
+        </GradientText>
         <ul className="grid md:grid-cols-2 lg:grid-cols-2 gap-x-[24px] gap-y-[40px] mt-[88px]">
-          {blockExplors.map((data) => (
+          {blockExplorers.map((data) => (
             <li key={data.title} className="min-w-[268px] max-w-[324px]">
               <PluginItem {...data} />
             </li>
@@ -170,7 +73,9 @@ const AppsPage = () => {
         </ul>
       </section>
       <section className="flex flex-col items-center mt-[88px] p-[24px]">
-        <GradientText tag="h3" className="text-[32px] text-center">Tools and plugins</GradientText>
+        <GradientText tag="h3" className="text-[32px] text-center">
+          Tools and plugins
+        </GradientText>
         <ul className="grid md:grid-cols-2 lg:grid-cols-2 gap-x-[24px] gap-y-[40px] mt-[88px]">
           {plugins.map((data) => (
             <li key={data.title} className="w-[324px]">
@@ -180,7 +85,34 @@ const AppsPage = () => {
         </ul>
       </section>
     </Layout>
-  )
-}
+  );
+};
 
-export default AppsPage
+export default function AppsPageData(props) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query AppListQuery {
+          appList: markdownRemark(
+            fileAbsolutePath: { regex: "/src/content/appList.md$/" }
+          ) {
+            frontmatter {
+              en {
+                appItems {
+                  code
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={(data) => {
+        const {
+          appItems: { code: appListCode },
+        } = data.appList.frontmatter.en;
+        const appListData = JSON.parse(appListCode);
+        return <AppsPage items={appListData} {...props} />;
+      }}
+    />
+  );
+}
