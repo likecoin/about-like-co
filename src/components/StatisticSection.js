780ca69db4399b4e1ef6e04f6cc488a98d76b55c
Ng Wing Tat, David <i@ngwingt.at>
2023-07-03 16:42:56 +0800

    💄 Update statistic section

--- src/components/StatisticSection.js
+++ src/components/StatisticSection.js
@@ -6,8 +6,6 @@ export default function StatisticSection() {
   const [registeredContents, setRegisteredContents] = React.useState(0)
   const [writingNFTCount, setWritingNFTCount] = React.useState(0)
   const [walletsCount, setWalletsCount] = React.useState(0)
-  const [integratedSites, setIntegratedSites] = React.useState(0)
-  const [creatorsCount, setCreatorsCount] = React.useState(0)
   const [likeDistribution, setLikeDistribution] = React.useState(0)
   React.useEffect(() => {
     async function fetchData() {
@@ -15,17 +13,13 @@ export default function StatisticSection() {
         const {
           totalISCNCount: registeredContentsRaw = 0,
           NFTCount: writingNFTCountRaw = 0,
-          totalLikee: creatorsCountRaw = 0,
           LIKEs: likeDistributionRaw = 0,
           totalAddresses: walletsCountRaw = 0,
-          totalSites: integratedSitesRaw = 0,
         } = await fetch("https://like.co/api/like/like/stat").then((r) => r.json())
         setRegisteredContents(registeredContentsRaw)
         setWritingNFTCount(writingNFTCountRaw)
-        setCreatorsCount(creatorsCountRaw)
         setLikeDistribution(likeDistributionRaw)
         setWalletsCount(walletsCountRaw)
-        setIntegratedSites(integratedSitesRaw);
       } catch (error) {
         console.error("Failed to fetch stat number")
         console.error(error)
@@ -39,43 +33,37 @@ export default function StatisticSection() {
         <ul className="mx-auto my-14 w-full lg:max-w-[1024px] xl:max-w-[1440px] min-w-[224px] gap-x-[96px] gap-y-[48px] grid md:grid-cols-2 xl:grid-cols-3 sm:flex-row flex-wrap justify-center">
           <li>
             <StatNumber
-              value={walletsCount || 24000}
+              value={walletsCount || 103245}
               unit="likers"
-              label="Addresses with LIKE"
+              label="wallet addresses with LIKE"
             />
           </li>
           <li>
             <StatNumber
-              value={integratedSites || 7000}
-              unit="media sites"
-              label="LikeCoin Widget loaded"
-            />
-          </li>
-          <li>
-            <StatNumber
-              value={registeredContents || 683384}
-              unit="content"
-              label="Content registered in the LikeCoin network"
-            />
-          </li>
-          <li>
-            <StatNumber
-              value={creatorsCount || 19000}
+              value={18140}
               unit="creators"
-              label="Liker ID with LikeCoin Widget"
+              label="have registered content in the LikeCoin network"
             />
           </li>
           <li>
             <StatNumber
-              value={likeDistribution || 48570000}
-              unit="LIKE"
-              label="Distributed LIKE"
+              value={registeredContents || 2361362}
+              unit="content"
+              label="are registered in the LikeCoin network"
             />
           </li>
           <li>
             <StatNumber
-              value={writingNFTCount || 1300}
+              value={writingNFTCount || 13147}
               unit="Writing NFT"
+              label="are minted from content"
+            />
+          </li>
+          <li>
+            <StatNumber
+              value={likeDistribution || 55769330}
+              unit="LIKE"
+              label="are distributed to creators"
             />
           </li>
         </ul>
